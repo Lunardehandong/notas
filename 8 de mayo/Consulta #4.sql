1982f7e649afd4c3a56f811a8e3fe07cdaca17d3
Diego Sánchez <154391492+Lunardehandong@users.noreply.github.com>
2026-05-08 05:42:00 -0600

    Add files via upload

---
+++ 8 de mayo/Consulta #4.sql
@@ -0,0 +1,6 @@
+INSERT INTO alumnos(Alumno_Nombre) VALUES
+('Mario hugo'),('Luis cabrera'),('Harry potter')
+
+INSERT INTO productos (Prod_Descripcion,Prod_Color) VALUES
+('mi producto 2','mi color 2'), ('mi producto 3','mi color 3'), ('mi producto 4','mi color 4');
+
