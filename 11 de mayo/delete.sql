4461b637c27903f7a57043d535c4a28cc487d8eb
Diego Sánchez <154391492+Lunardehandong@users.noreply.github.com>
2026-05-11 04:32:21 -0600

    Add files via upload

---
+++ 11 de mayo/delete.sql
@@ -0,0 +1,23 @@
+//hora de borrar
+
+DELETE FROM productos p
+
+//donde NO le pongas WHERE borras 
+toda la tabla
+
+//primer registro de borrar
+DELETE FROM productos p WHERE
+p.Prod_Id>6992
+
+//resumen de la consultas consultas
+INSERT INTO <tabla>
+(<campos>) VALUES (<valores>)
+
+UPDATE <tabla> SET <campo>=<valor> 
+WHERE
+<condicion o condiciones>
+
+DELETE FROM <tabla> WHERE 
+<condicion o
+condiciones>
+
